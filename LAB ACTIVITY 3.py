04965c7a483b0dcb6d367fa215cfdbffdc61a200
RHAndrion <rhaneilheinrichandrion@gmail.com>
2025-03-11 22:18:03 +0800

    Add files via upload

---
+++ LAB ACTIVITY 3.py
@@ -0,0 +1,13 @@
+#LAB ACTIVITY 3
+def check_number(num):
+    if num % 2 == 0:
+        print(f"{num} is an Even number.")
+    else:
+        print(f"{num} is an Odd number.")
+
+# Test Numbers
+check_number(4)   
+check_number(7)   
+check_number(0)  
+check_number(-10)  
+check_number(34)   
